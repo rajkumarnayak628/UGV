0f7411635408b5094cd65aa4f3c291c6909179ad
snape <snape@github.com>
2018-03-15 18:12:00 +0530

    final

--- js/main.js
+++ js/main.js
@@ -23,6 +23,12 @@ jQuery(document).ready(function( $ ) {
     },
     speed: 400
   });
+    
+    
+    
+   
+    
+    
 
   // Mobile Navigation
   if ($('#nav-menu-container').length) {
@@ -168,3 +174,19 @@ jQuery(document).ready(function( $ ) {
   });
 
 });
+
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"; 
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1} 
+    slides[slideIndex-1].style.display = "block"; 
+    setTimeout(showSlides, 1500); // Change image every 2 seconds
+}
